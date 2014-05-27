d1885b560a375635ec4527eaf1e3c33cc0ef6672
rcsv <Awane.Tomohiro@gmail.com>
2014-05-28 08:10:07 +0900

    とちゅうかけ。

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-cakephp-pickers-collection
-==========================
+# Picker Collection
+cakephp-picker-collection support you generate many types of picker in your
+web application.
 
-Cakephp 用 jQuery + Bootstrap Form Picker 集
+## 日本語による説明でごあす。
+Picker は CakePHP のプラグインとして動作するピッカーコレクションです。
+使えるピッカーは３種類で、それぞれ:
+
+1. [x][Colorpicker]() jquery.minicolors.js です。
+2. [ ][Date / Time Datetime picker]() bootstrap-datetimepicker.min.js です。
+3. [ ][Timezonepicker]() 未実装です。
+
+なお、Twitter Bootstrap ヘルパープラグインの BoostCake を必須としています。
+
+## インスコ
+composer でインスコ。
+
+## 設置
+`app/Config/bootstrap.php` で当該プラグインを読み込みます。
+
+    CakePlugin::loadAll();
+
+    // or
+
+    CakePlugin::load('Picker');
+
+次に、`app/Controller/AppController.php` または他のコントローラ、ヘルパー等で、
+下記の様に使用宣言します。
+
+    public $helpers = array(
+      'Picker.Picker' => array(
+        'jsfiles' => array(
+          'jquery' => 'http://....../jquery.min.js',
+          'bootstrap' => '......',
+          'color' => '.....',
+          'date' => '.....',
+          'time' => '.....',
+          'datetime' => '.....',
+          'timezone' => '.....'),
+        'cssfiles' => array(
+          'bootstrap' => 'bootstrap.min',
+          'color' => 'jquery.minicolors',
+          'date' => 'bootstrap-datetimepicker.min',
+          'time' => 'bootstrap-datetimepicker.min',
+          'datetime' => 'bootstrap-datetimepicker.min'
+          )));
+
+## 使い方
+`app/View/MODEL/hoge.ctp` にて、下記の用に呼び出し、`FormHelper` と同じ様に使用します。
+
+    echo $this->Form->create('PickerCollection');
+    echo $this->Form->input('title');
+    echo $this->Picker->color('bgcolor', array('class' => 'hoge'));
+    echo $this->Picker->date('start', array('placeholder' => '2014-08-31'));
+    echo $this->Picker->time('time', array('placeholder' => '18:30:00'));
+    echo $this->Picker->datetime('modified', array('placeholder' => '2014-06-20 20:00:00'));
+    echo $this->Picker->timezone('place', array('place' => 'Asia/Tokyo'));
--- View/Helper/PickerHelper.php
+++ View/Helper/PickerHelper.php
@@ -5,90 +5,30 @@ App::uses('HtmlHelper', 'View/Helper');
 /**
  * Picker Helper
  *
- * Generate form input
+ * Generate data pickers with HTML FORM INPUT + javascript.
+ *
+ *
+ * ## How to use
+ *
+ * Load plugin using `CakePlugin::load()` method in <code>app/Config/bootstrap.php</code>
+ *    ~~~CakePlugin::load('Picker');
+ *    ~~~
+ * or
+ *
+ *    ~~~CakePlugin::loadAll();
+ *    ~~~
+ *
+ *
+ * @package  Picker.View.Helper
+ * @author   rcsvpg@gmail.com
+ * @link     http://github.com/rcsv/pickers-collection
  */
-class PickerHelper extends AppHelper {
+class PickerHelper extends BoostCakeFormHelper {
 
-	// pickers DEFAULT OPTIONS
-	public $pickersDefault = array(
-		'div' => array(
-			'class' => 'form-group'),
-		'type' => 'text',
-		'class' => 'form-control');
-
-	// javascript source files
-	public $jsfiles = array(
-		'jquery' => 'jquery-2.1.0.min',
-		'bootstrap' => 'bootstrap.min',
-		'color' => 'jquery.minicolors'
-	);
-
-	// css files
-	public $cssfiles = array(
-		'bootstrap' => 'bootstrap.min',
-		'color' => 'jquery.minicolors'
-	);
-
-	public $helpers = array('Form', 'Html');
-
-	// Constructor
-	public function __construct($View, $settings = array()) {
-		$this->serial_number = 1;
-		parent::__construct($View, $settings);
-	}
-
-	/**
-	 * Generate ColorPicker via bootstrap-colorpicker
-	 *
-	 * @param string $fieldName form field name
-	 * @param array $options options
-	 */
-	public function color($fieldName, $options = array()) {
-		$this->loadfiles(array('jquery', 'bootstrap', 'color');
-		$pickerOption = json_encode(isset($options['pickerOption']) ? $options['pickerOption'] : array(), JSON_FORCE_OBJECT);
-		unset($options['pickerOption']);
-		$this->merge_options($options);
-		$this->options = $this->appendClass('minicolors');
-		$this->options['wrapInput'] = false;
-		echo $this->Html->scriptBlock("\$('input.minicolors).minicolors(${pickerOption})", self::$AIF);
-		return $this->Form->input($fieldName, $this->options);
-	}
-
-	// PRIVATE FUNCTIONS
-	// --------------------------------------------------------------------
-
-	private function merge_options($options = array()) {
-		$this->options = array_merge($this->pickersDefault, $options);
-		$options = $this->appendClass('form-control');
-	}
-
-	private function appendClass($classValue) {
-		if (empty($this->options['class']) || strstr($this->options['class'], $classValue) === false) {
-			$this->options['class'] .= " ${classValue}";
-		} else {
-			$this->options['class'] = $classValue;
-		}
-		return $this->options;
-	}
-
-	private function loadfiles($sources = array()) {
-		foreach ($sources as $source) {
-			if (!empty($this->jsfiles[$source])) {
-				echo $this->Html->script(strpos($this->jsfiles[$source], '//') ==== 0 ? '' : 'Picker.' . $this->jsfiles[$source], self::$AIF);
-			}
-			if (!empty($this->cssfiles[$source])) {
-				echo $this->Html->css(strpos($this->cssfiles[$source], '//') === 0 ? '' : 'Picker.' . $this->cssfiles[$source], self::$AIF);
-			}
-		}
-	}
-
-	private function get_serial() {
-		return $this->prefix_id . $this->serial++;
-	}
-
-	private $serial;
-	private $prefix_id = 'picker';
-	private $options = array();
-
-	// SHORTHAND of INLINE FALSE
-	private static $AIF = array('inline' => false);
+  // Helpers what I am going to use, listed below.
+  // Pickers requires BoostCake plugin.
+  public $helpers = array(
+    'Picker.PickerHtml',
+    'Form' => array('className' => 'BoostCake.BoostCakeForm'),
+    'Html' => array('className' => 'BoostCake.BoostCakeHtml'));
+}
