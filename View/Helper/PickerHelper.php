4522dbfc01c0950ff5ccbc48398525ae228c4c28
rcsv <Awane.Tomohiro@gmail.com>
2014-05-30 07:16:38 +0900

    コメント拡充した

--- View/Helper/PickerHelper.php
+++ View/Helper/PickerHelper.php
@@ -108,12 +108,18 @@ class PickerHelper extends BoostCakeFormHelper {
 		'bootstrap'			=> '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js', 
 		'jasny-bootstrap'	=> '//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js', 
 		'color' 			=> 'Picker.jquery.minicolors.min', 
-		'moment' 			=> '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js',  // moment.js required >= 2.5.1 by datetimepicker
-		'moment.ja' 		=> '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/lang/ja.js', 
+		// moment.js required >= 2.5.1 by datetimepicker
+		'moment' 			=> '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js',  
+		// 'moment.ja' 		=> '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/lang/ja.js', 
 		'date' 				=> '//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/3.0.0/css/bootstrap-datetimepicker.min.css', 
-		'date.ja' 			=> 'Picker.locales/bootstrap-datetimepicker.ja',
+		// 'date.ja' 			=> 'Picker.locales/bootstrap-datetimepicker.ja',
 		'gmaps' 			=> 'http://maps.google.com/maps/api/js?sensor=false&libraries=places', 
-		'location' 			=> 'Picker.locationpicker.jquery', 
+		// Location Picker :: http://logicify.github.io/jquery-locationpicker-plugin/
+		'location' 			=> 'Picker.locationpicker.jquery',
+		
+		// http://mngscl-10.s3-website-us-east-1.amazonaws.com/jquery-addresspicker-bootstrap/demos/index.html
+		'address'			=> '', // https://github.com/sgruhier/jquery-addresspicker 
+		
 		'timezone' 			=> '//');
 	///@formatter:on
 	
@@ -141,16 +147,29 @@ class PickerHelper extends BoostCakeFormHelper {
 	
 
 	/**
-	 * Constructor stores new paths of .
+	 * Constructor will prepare javascript and css file list for call them.
 	 *
-	 *
-	 *
-	 *
-	 * js and .css
-	 *
+	 * ### Default Values
+	 * $settings allows two arrays 'jsfiles' and 'cssfiles'.
+	 * 
+	 * {{{
 	 * public $helpers = array(
-	 * 'Picker.Picker' => array('jsfiles' => array('')));
+	 * 		'Picker.Picker' => array(
+	 * 
+	 * 			// default values of javascript files
+	 * 			'jsfiles' => array(
+	 * 				'jquery' => 'Picker.jquery-2.1.0.min',
+	 * 				'bootstrap' => 'Picker.bootstrap.min',
+	 * 				'color' => 'Picker.jquery.minicolors',
+	 * 				'date' => 'Picker.bootstrap-datetimepicker.min'),
+	 * 
+	 * 			// default values of CSS files
+	 * 			'cssfiles' => array(
+	 * 				'bootstrap' => 'Picker.bootstrap.min',
+	 * 				'color' => 'Picker.jquery.minicolors',
+	 * 				'date' => 'Picker.bootstrap-datetimepicker.min'))));
 	 *
+	 * }}}
 	 * @param object $View an instance of CakeView? Object.
 	 * @param array $settings Parameters set at AppController::$helpers array.
 	 */
@@ -166,10 +185,28 @@ class PickerHelper extends BoostCakeFormHelper {
 
 	/**
 	 * Generate input tag and enabled Colorpicker.
-	 * See
-	 * http://labs.abeautifulsite.net/jquery-minicolors/.
-	 *
-	 *
+	 * See a [demo page](http://labs.abeautifulsite.net/jquery-minicolors/).
+	 * 
+	 * {{{
+	 * echo $this->Picker->color('Color', array(
+	 *		'class' => 'a',
+	 *	 	'pickerOption' => array(
+	 *			'animationSpeed' => 50,
+	 *			'animationEasing' => 'swing',
+	 *			'change' => null,
+	 *			'changeDelay' => 0,
+	 *			'control' => 'hue' || 'brightness' || 'saturation' || 'wheel',
+	 *			'defaultValue' => '',
+	 *			'hide' => null,
+	 *			'hideSpeed' => 100,
+	 *			'inline' => false,
+	 *			'letterCase' => 'lowercase' || 'uppercase',
+	 *			'opacity' => false,
+	 *			'position' => 'bottom left',
+	 *			'show' => null,
+	 *			'showSpeed' => 100,
+	 *			'theme'	=> 'default' || 'bootstrap')));
+	 * }}}
 	 *
 	 * @param string $fieldName a fieldname.
 	 * @param array $options options array.
@@ -177,15 +214,21 @@ class PickerHelper extends BoostCakeFormHelper {
 	public function color($fieldName, $options = array()) {
 
 		$options = array_merge($this->colorpickerDefault, $options);
-		$pickerOption = json_encode(isset($options['pickerOption']) ? $options['pickerOption'] : array(), JSON_FORCE_OBJECT |
-			 JSON_PRETTY_PRINT);
+		$pickerOption = json_encode(
+			isset($options['pickerOption']) ? $options['pickerOption'] : array(),
+			JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
 		unset($options['pickerOption']);
+		
 		$options['class'] = isset($options['class']) &&
-			 strstr($options['class'], 'minicolors form-control') === false ? "${options['class']} minicolors form-control" : "minicolors form-control";
+			 strstr($options['class'], 'minicolors form-control') === false 
+				? "${options['class']} minicolors form-control" 
+				: "minicolors form-control";
 		
 		$this->loadFiles(array(
 			'jquery', 'bootstrap', 'color'));
-		echo $this->Html->scriptBlock("\$('input.minicolors').minicolors(${pickerOption});", self::$AIF);
+		echo $this->Html->scriptBlock(
+			"\$('input.minicolors').minicolors(${pickerOption});",
+			self::$AIF);
 		return $this->Form->input($fieldName, $options);
 	}
 
@@ -199,11 +242,12 @@ class PickerHelper extends BoostCakeFormHelper {
 	public function location($fieldName, $options = array()) {
 
 		$options = array_merge($this->locationpickerDefault, $options);
-		$options['pickerOption'] = array_merge(!empty($options['pickerOption']) ? $options['pickerOption'] : array(), array(
-			'style' => 'width:500px;height:380px;', 
-			'inputBinding' => array(
-				
-				'locationNameInput' => "\$('#" . $this->domId($fieldName) . "')'")));
+		$options['pickerOption'] = array_merge(
+			!empty($options['pickerOption']) ? $options['pickerOption'] : array(), 
+			array(
+				'style' => 'width:500px;height:380px;', 
+				'inputBinding' => array(
+					'locationNameInput' => "\$('#" . $this->domId($fieldName) . "')'")));
 		
 		if (strstr($options['class'], 'form-control') === false) {
 			$options['class'] = "${options['class']} form-control";
@@ -216,8 +260,10 @@ class PickerHelper extends BoostCakeFormHelper {
 		
 		$this->loadFiles(array(
 			'jquery', 'bootstrap', 'gmaps', 'location'));
-		echo $this->Html->scriptBlock("\$('#${divId}').locationpicker(" . preg_replace('/"*"/', '$1', json_encode($options['pickerOption'], JSON_FORCE_OBJECT |
-			 JSON_PRETTY_PRINT)), self::$AIF);
+		echo $this->Html->scriptBlock("\$('#${divId}').locationpicker(" 
+			. preg_replace('/"*"/', '$1', json_encode($options['pickerOption'], 
+				JSON_FORCE_OBJECT | JSON_PRETTY_PRINT)),
+			self::$AIF);
 		return $maparea . $this->Form->input($fieldName, $options);
 	}
 
@@ -230,8 +276,10 @@ class PickerHelper extends BoostCakeFormHelper {
 	 */
 	public function date($fieldName, $options = array()) {
 
-		$options['pickerOption'] = array_merge(!empty($options['pickerOption']) ? $options['pickerOption'] : array(), array(
-			'pickDate' => true, 'pickTime' => false));
+		$options['pickerOption'] = array_merge(
+			!empty($options['pickerOption']) ? $options['pickerOption'] : array(), 
+			array(
+				'pickDate' => true, 'pickTime' => false));
 		return $this->generateDateTimePicker($fieldName, $options);
 	}
 
@@ -244,11 +292,17 @@ class PickerHelper extends BoostCakeFormHelper {
 	 */
 	public function time($fieldName, $options = array()) {
 
-		$option = array_merge(array(
-			
-			'beforeInput' => '<span class="input-group-addon"><i class="add-on glyphicon glyphicon-time"></i></span>'), $options);
-		$options['pickerOption'] = array_merge(!empty($option['pickerOption']) ? $option['pickerOption'] : array(), array(
-			'pickDate' => false, 'pickTime' => true));
+		$option = array_merge(
+			array(
+				'beforeInput' => '<span class="input-group-addon"><i class="add-on glyphicon glyphicon-time"></i></span>'), 
+			$options);
+		
+		$options['pickerOption'] = array_merge(
+			!empty($option['pickerOption']) ? $option['pickerOption'] : array(), 
+			array(
+				'pickDate' => false, 
+				'pickTime' => true));
+		
 		return $this->generateDateTimePicker($fieldName, $option);
 	}
 
@@ -265,16 +319,30 @@ class PickerHelper extends BoostCakeFormHelper {
 	 */
 	public function dateAndTime($fieldName, $options) {
 
-		$options['pickerOption'] = array_merge(array(
-			'sideBySide' => true), !empty($options['pickerOption']) ? $options['pickerOption'] : array(), array(
-			'pickDate' => true, 'pickTime' => true));
+		$options['pickerOption'] = array_merge(
+			array(
+				'sideBySide' => true),
+			!empty($options['pickerOption']) ? $options['pickerOption'] : array(), 
+			array(
+				'pickDate' => true, 
+				'pickTime' => true));
+		
 		return $this->generateDateTimePicker($fieldName, $options);
 	}
 	
 	// TimeZone Picker DOES NOT IMPLEMENTS YET
 	public function timezone($fieldName, $options = array()) {
 
-		throw new NotImplementedException('PickerHelper::timezone picker does not implement yet.');
+		throw new NotImplementedException(
+			'PickerHelper::timezone picker does not implement yet.');
+		
+	}
+	
+	public function address($fieldName, $options = array()) {
+		
+		throw new NotImplemetedException(
+			'PickerHelper::timezone picker does not implement yet.'
+		);
 	}
 	
 	// Private methods
@@ -306,14 +374,20 @@ class PickerHelper extends BoostCakeFormHelper {
 		
 		$divId = $this->getSerial();
 		$options['wrapInput']['id'] = $divId;
-		echo $this->Html->scriptBlock("\$(function () { \$('#${divId}').datetimepicker(" . json_encode($options['pickerOption'], JSON_FORCE_OBJECT |
-			 JSON_PRETTY_PRINT) . ")});", self::$AIF);
+		echo $this->Html->scriptBlock(
+			"\$(function () { \$('#${divId}').datetimepicker(" 
+				. json_encode($options['pickerOption'],
+					JSON_FORCE_OBJECT | JSON_PRETTY_PRINT) 
+				. ")});",
+			self::$AIF);
+		
 		unset($options['pickerOption']);
 		return $this->Form->input($fieldName, $options);
 	}
 	
-	// For surpress duplicate call.
-	// an array to store resource name already called by loadFiles() method.
+	
+	// an array to store resource names already called by FormHelper::css() 
+	// method. It is for surpress duplicate call CSS link tag.
 	private $alreadyLoadedCSS = array();
 
 
@@ -361,6 +435,5 @@ class PickerHelper extends BoostCakeFormHelper {
 	private $prefix = 'picker';
 	
 	// SHORT HAND of INLINE => FALSE
-	private $AIF = array(
-		'inline' => false);
+	private $AIF = array('inline' => false);
 }
